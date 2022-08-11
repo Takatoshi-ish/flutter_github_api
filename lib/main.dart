db8eebe6189634425cc165ff74e9652df61afd9d
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-11 12:51:08 +0900

    launchUrlStringを追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,33 +40,13 @@ class _GithubPageState extends State<GithubPage> {
       queryParameters: {
         'sort': 'stars',
         'q': text,
-        'per_page': 20,
+        'per_page': 100,
       },
     );
     final List items = response.data['items'];
     debugPrint('items: $items');
     githubRepo = items.map((e) => GithubRepo.fromMap(e)).toList();
     setState(() {});
-  }
-
-  Future<void> shareImage(String url) async {
-    // まずは一時保存に使えるフォルダ情報を取得します。
-    // Future 型なので await で待ちます
-    final dir = await getTemporaryDirectory();
-
-    final response = await Dio().get(
-      url,
-      options: Options(
-        // 画像をダウンロードするときは ResponseType.bytes を指定します。
-        responseType: ResponseType.bytes,
-      ),
-    );
-    // フォルダの中に image.png という名前でファイルを作り、そこに画像データを書き込みます。
-    final imageFile =
-        await File('${dir.path}/image.png').writeAsBytes(response.data);
-
-    // path を指定すると share できます。
-    await Share.shareFiles([imageFile.path]);
   }
 
   // この関数の中の処理は初回に一度だけ実行されます。
@@ -96,36 +78,40 @@ class _GithubPageState extends State<GithubPage> {
                   border: Border(
                       bottom: BorderSide(width: 1.0, color: Colors.grey))),
               child: ListTile(
-                title: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        githubRepo[index].avatarUrl,
+                title: GestureDetector(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          githubRepo[index].avatarUrl,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 15),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '${githubRepo[index].name}',
-                          style: TextStyle(color: Colors.black, fontSize: 14.0),
-                        ),
-                        Text(
-                          '${githubRepo[index].starCount}',
-                          style: TextStyle(color: Colors.black, fontSize: 18.0),
-                        ),
-                      ],
-                    ),
-                  ],
+                      const SizedBox(width: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${githubRepo[index].name}',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 14.0),
+                          ),
+                          Text(
+                            '${githubRepo[index].starCount}',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18.0),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    launchUrlString(githubRepo[index].htmlUrl);
+                  }, // タップ
+                  onLongPress: () {
+                    print("onLongTap called.");
+                  }, // 長押し
                 ),
-                onTap: () {
-                  print("onTap called.");
-                }, // タップ
-                onLongPress: () {
-                  print("onLongTap called.");
-                }, // 長押し
               ),
             );
           },
@@ -139,11 +125,13 @@ class GithubRepo {
   final String name;
   final int starCount;
   final String avatarUrl;
+  final String htmlUrl;
 
   GithubRepo({
     required this.name,
     required this.starCount,
     required this.avatarUrl,
+    required this.htmlUrl,
   });
 
   factory GithubRepo.fromMap(Map<String, dynamic> map) {
@@ -151,6 +139,7 @@ class GithubRepo {
       name: map['full_name'] ?? '',
       starCount: map['stargazers_count'] ?? 0,
       avatarUrl: map['owner']['avatar_url'] ?? '',
+      htmlUrl: map['html_url'] ?? '',
     );
   }
 }
